5861f66617b295dea0408256b1dee3d06672fa7c
crazycoder44 <crazycoder44@gmail.com>
2025-10-31 16:52:38 +0100

    Add cached property list at /properties/ (cached 15 minutes)

---
+++ properties/urls.py
@@ -0,0 +1,8 @@
+# properties/urls.py
+
+from django.urls import path
+from .views import property_list
+
+urlpatterns = [
+    path("", property_list, name="property-list"),  # /properties/
+]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# properties/views.py
 
-# Create your views here.
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .models import Property
+
+# Cache this view's entire response for 15 minutes (60 * 15 seconds)
+@cache_page(60 * 15)
+def property_list(request):
+    """
+    Return a JSON list of all properties.
+    Cached in Redis for 15 minutes.
+    """
+    qs = Property.objects.all().order_by('-created_at')
+    # Use values() to cheaply serialize the fields we want
+    data = list(
+        qs.values(
+            "id", "title", "description", "price", "location", "created_at"
+        )
+    )
+    return JsonResponse({"count": len(data), "results": data})
